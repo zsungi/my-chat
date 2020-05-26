94e0b28e6b13cab6cc21a610cb374423122347b7
Kégl Marcell <keglmarcell@Kegls-MacBook-Pro.local>
2020-05-26 16:47:15 +0200

    add build again

---
+++ build/precache-manifest.766d318ed4b0611263bd78a9ed199d51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c47a7533339ddd9a3b8b79c2ce56877",
+    "url": "./index.html"
+  },
+  {
+    "revision": "72e31991d07ee4a88a3a",
+    "url": "./static/css/main.ecdc7a5d.chunk.css"
+  },
+  {
+    "revision": "f9f1c85ed2dfb059483d",
+    "url": "./static/js/2.db39bea0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.db39bea0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72e31991d07ee4a88a3a",
+    "url": "./static/js/main.3ccf87c2.chunk.js"
+  },
+  {
+    "revision": "c999e1aafeacb3931724",
+    "url": "./static/js/runtime-main.9edb4eab.js"
+  }
+]);
